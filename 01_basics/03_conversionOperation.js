a586ad6806892d106269432a0fd790601310272c
Parvez Ahmed <114897117+Parvez7421@users.noreply.github.com>
2025-03-15 17:56:48 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score =true//undefined //null//"33abc"
+console.log(typeof score)
+console.log(typeof(score))
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33"==>33
+//33abc==>NaN
+//true ==>1 false==>0
+let isLoggedIn ="par"//""//1
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1==>true ;0==>false
+//""==>false
+//"par"==>true
+let sumNumber = 33;
+let stringNumber = String(sumNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
